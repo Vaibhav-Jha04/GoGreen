3ebd1df2e036206101473d3e366c79fe83842d2d
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-01-20 09:09:08 +1000

    feat: add dark mode preference storage

--- src/validators.js
+++ src/validators.js
@@ -47,3 +47,5 @@ export const isAlphanumeric = (v) => /^[a-zA-Z0-9]+$/.test(v);
 export const hasKeys = (obj, keys) => keys.every(k => Object.prototype.hasOwnProperty.call(obj, k));
 
 export const hasKeys = (obj, keys) => keys.every(k => Object.prototype.hasOwnProperty.call(obj, k));
+
+export const inRange = (v, min, max) => typeof v === 'number' && v >= min && v <= max;
