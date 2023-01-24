7dbdb0c1bc19faf319e26adbcd3eee0cf0cf44cb
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-01-24 16:28:49 +1000

    feat: add email digest preferences

--- src/utils.js
+++ src/utils.js
@@ -132,3 +132,9 @@ export const noop = () => {};
 
 export const zip = (...arrays) =>
   Array.from({ length: Math.min(...arrays.map(a => a.length)) }, (_, i) => arrays.map(a => a[i]));
+
+export const chunk = (arr, size) => {
+  const out = [];
+  for (let i = 0; i < arr.length; i += size) out.push(arr.slice(i, i + size));
+  return out;
+};
