c0e22fa76d064194d82fe142a89697135eeec7fb
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-02-16 17:50:41 +1000

    feat: add progress indicator for long operations

--- src/formatters.js
+++ src/formatters.js
@@ -72,3 +72,5 @@ export const formatCurrency = (amount, currency = 'USD', locale = 'en-US') =>
 
 export const toCamelCase = (str) =>
   str.replace(/[-_\s]+(.)?/g, (_, c) => (c ? c.toUpperCase() : ''));
+
+export const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
