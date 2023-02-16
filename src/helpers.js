31e9a59ea507ab86c1a39782b4592858b48683a0
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-02-16 13:21:47 +1000

    fix: stop infinite re-render loop in effect hook

--- src/helpers.js
+++ src/helpers.js
@@ -206,3 +206,10 @@ export const batchProcess = async (items, fn, batchSize = 10) => {
   }
   return results;
 };
+
+export const groupBy = (arr, key) =>
+  arr.reduce((acc, item) => {
+    const g = typeof key === 'function' ? key(item) : item[key];
+    (acc[g] = acc[g] || []).push(item);
+    return acc;
+  }, {});
