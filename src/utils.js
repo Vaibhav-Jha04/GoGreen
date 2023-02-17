a90e96eb9e215d8be01d9ee3b2105b3ac0d3965b
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-02-17 11:13:19 +1000

    fix: resolve null reference in data processing module

--- src/utils.js
+++ src/utils.js
@@ -149,3 +149,5 @@ export const uniq = (arr) => [...new Set(arr)];
 export const uniq = (arr) => [...new Set(arr)];
 
 export const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+export const noop = () => {};
