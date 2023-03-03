6b132c3fc89e32e49594268d8e3c131d85da8d5d
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-03-03 17:55:12 +1000

    fix: resolve null reference in data processing module

--- src/utils.js
+++ src/utils.js
@@ -180,3 +180,9 @@ export const deepClone = (val) => {
 export const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
 export const toArray = (val) => (Array.isArray(val) ? val : [val]);
+
+export const chunk = (arr, size) => {
+  const out = [];
+  for (let i = 0; i < arr.length; i += size) out.push(arr.slice(i, i + size));
+  return out;
+};
