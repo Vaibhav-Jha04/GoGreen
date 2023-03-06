ca55f0b0ada75c4a7132ef853e1b072cd1554ccf
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-03-06 19:11:50 +1000

    feat: implement retry logic with exponential backoff

--- src/validators.js
+++ src/validators.js
@@ -68,3 +68,5 @@ export const isUUID = (v) =>
   /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(v);
 
 export const maxLength = (v, n) => typeof v === 'string' && v.length <= n;
+
+export const maxLength = (v, n) => typeof v === 'string' && v.length <= n;
