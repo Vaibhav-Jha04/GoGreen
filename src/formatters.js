328b51a2081cbced22b84d2d0825acccb21e0b55
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-03-09 19:11:30 +1000

    feat: add two-factor authentication flow

--- src/formatters.js
+++ src/formatters.js
@@ -90,3 +90,6 @@ export const formatNumber = (n, decimals = 2) =>
 
 export const truncate = (str, max, ellipsis = '…') =>
   str.length <= max ? str : str.slice(0, max - ellipsis.length) + ellipsis;
+
+export const formatCurrency = (amount, currency = 'USD', locale = 'en-US') =>
+  new Intl.NumberFormat(locale, { style: 'currency', currency }).format(amount);
