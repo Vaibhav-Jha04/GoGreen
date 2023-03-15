5b7487fc436b1cef8023bce8dcfdbe91694a677e
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-03-15 14:19:19 +1000

    fix: correct header capitalization in HTTP client

--- src/api.js
+++ src/api.js
@@ -123,3 +123,11 @@ export const del = async (path, options = {}) => {
   if (!res.ok) throw new Error(`DELETE ${path} failed: ${res.status}`);
   return res.status === 204 ? null : res.json();
 };
+
+const BASE_URL = process.env.API_URL || 'http://localhost:3000';
+
+export const get = async (path, options = {}) => {
+  const res = await fetch(`${BASE_URL}${path}`, { ...options, method: 'GET' });
+  if (!res.ok) throw new Error(`GET ${path} failed: ${res.status}`);
+  return res.json();
+};
