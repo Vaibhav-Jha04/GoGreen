3127fcdffa9bb0956f9f44ef277958077ff457bd
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-03-23 10:03:37 +1000

    fix: stop stale cache from serving outdated responses

--- src/validators.js
+++ src/validators.js
@@ -74,3 +74,5 @@ export const maxLength = (v, n) => typeof v === 'string' && v.length <= n;
 export const isUrl = (v) => { try { new URL(v); return true; } catch { return false; } };
 
 export const isEmail = (v) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+
+export const hasKeys = (obj, keys) => keys.every(k => Object.prototype.hasOwnProperty.call(obj, k));
