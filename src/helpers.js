62fbb2f6acff94513f592c326af10de77af6ed3e
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-03-25 10:15:10 +1000

    feat: add CSV import with validation

--- src/helpers.js
+++ src/helpers.js
@@ -296,3 +296,10 @@ export const throttle = (fn, limit) => {
 };
 
 export const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
+
+export const groupBy = (arr, key) =>
+  arr.reduce((acc, item) => {
+    const g = typeof key === 'function' ? key(item) : item[key];
+    (acc[g] = acc[g] || []).push(item);
+    return acc;
+  }, {});
