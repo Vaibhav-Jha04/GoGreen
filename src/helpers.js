66fbf7866c594cd085bb703002cb3effb5455625
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-04-13 17:49:11 +1000

    fix: escape special characters in regex builder

--- src/helpers.js
+++ src/helpers.js
@@ -323,3 +323,11 @@ export const memoize = (fn) => {
 };
 
 export const compose = (...fns) => (x) => fns.reduceRight((v, f) => f(v), x);
+
+export const retry = async (fn, retries = 3, delay = 500) => {
+  try { return await fn(); } catch (err) {
+    if (retries <= 0) throw err;
+    await new Promise(r => setTimeout(r, delay));
+    return retry(fn, retries - 1, delay * 2);
+  }
+};
