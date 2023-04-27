ebeda6e7aa39d3ca343f5e5fee14361c190435c2
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-04-27 16:50:44 +1000

    feat: add input validation layer for user payloads

--- src/validators.js
+++ src/validators.js
@@ -84,3 +84,6 @@ export const isInteger = (v) => Number.isInteger(v);
 export const isPositive = (v) => typeof v === 'number' && v > 0;
 
 export const isOneOf = (v, options) => options.includes(v);
+
+export const isUUID = (v) =>
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(v);
