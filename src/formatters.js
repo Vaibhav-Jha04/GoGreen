eaa1eb731ac9fedb5c787708f40d1a9bd0e486fa
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-05-01 13:14:18 +1000

    docs: add contributing guide

--- src/formatters.js
+++ src/formatters.js
@@ -107,3 +107,6 @@ export const toCamelCase = (str) =>
 
 export const toTitleCase = (str) =>
   str.replace(/\w\S*/g, (w) => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase());
+
+export const toCamelCase = (str) =>
+  str.replace(/[-_\s]+(.)?/g, (_, c) => (c ? c.toUpperCase() : ''));
