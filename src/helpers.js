a831bbbd09292c3ad51b4198d764a02cd33ee630
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-05-04 22:24:06 +1000

    chore: bump dependencies to latest stable versions

--- src/helpers.js
+++ src/helpers.js
@@ -14,3 +14,11 @@ export const retry = async (fn, retries = 3, delay = 500) => {
     return retry(fn, retries - 1, delay * 2);
   }
 };
+
+export const retry = async (fn, retries = 3, delay = 500) => {
+  try { return await fn(); } catch (err) {
+    if (retries <= 0) throw err;
+    await new Promise(r => setTimeout(r, delay));
+    return retry(fn, retries - 1, delay * 2);
+  }
+};
