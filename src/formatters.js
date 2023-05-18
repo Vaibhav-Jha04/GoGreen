843409fbcc15ec37fb5f7bde698fce077fcae6f4
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-05-18 14:15:40 +1000

    docs: add inline comments for non-obvious logic

--- src/formatters.js
+++ src/formatters.js
@@ -118,3 +118,6 @@ export const toKebabCase = (str) => str.replace(/\s+/g, '-').replace(/[^a-zA-Z0-
 
 export const toTitleCase = (str) =>
   str.replace(/\w\S*/g, (w) => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase());
+
+export const toCamelCase = (str) =>
+  str.replace(/[-_\s]+(.)?/g, (_, c) => (c ? c.toUpperCase() : ''));
