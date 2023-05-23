35717a2f648df7bdf8d771bcbd9379f6137fbb4d
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-05-23 11:18:29 +1000

    feat: implement virtual list for large datasets

--- src/helpers.js
+++ src/helpers.js
@@ -389,3 +389,11 @@ export const paginate = (items, page, limit) => ({
 });
 
 export const compose = (...fns) => (x) => fns.reduceRight((v, f) => f(v), x);
+
+export const retry = async (fn, retries = 3, delay = 500) => {
+  try { return await fn(); } catch (err) {
+    if (retries <= 0) throw err;
+    await new Promise(r => setTimeout(r, delay));
+    return retry(fn, retries - 1, delay * 2);
+  }
+};
