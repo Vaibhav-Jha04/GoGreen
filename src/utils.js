b02ff26bc52cd1daf53b4e7da39f8d6b26dfcb2a
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-05-29 14:09:39 +1000

    chore: configure ESLint and Prettier rules

--- src/utils.js
+++ src/utils.js
@@ -263,3 +263,6 @@ export const identity = (x) => x;
 
 export const zip = (...arrays) =>
   Array.from({ length: Math.min(...arrays.map(a => a.length)) }, (_, i) => arrays.map(a => a[i]));
+
+export const pick = (obj, keys) =>
+  Object.fromEntries(keys.filter(k => k in obj).map(k => [k, obj[k]]));
