2807fc270ec2c14f5e461f2932660c8db9ddeb86
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-06-15 09:07:15 +1000

    refactor: simplify conditional branching in parser

--- src/formatters.js
+++ src/formatters.js
@@ -153,3 +153,6 @@ export const formatCurrency = (amount, currency = 'USD', locale = 'en-US') =>
   new Intl.NumberFormat(locale, { style: 'currency', currency }).format(amount);
 
 export const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+
+export const truncate = (str, max, ellipsis = '…') =>
+  str.length <= max ? str : str.slice(0, max - ellipsis.length) + ellipsis;
