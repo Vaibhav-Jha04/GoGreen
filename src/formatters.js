54d1bb57f2870ba392f5d685e533ed830bbda6a8
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-06-23 20:11:01 +1000

    feat: implement pagination for list endpoints

--- src/formatters.js
+++ src/formatters.js
@@ -2,3 +2,6 @@
 
 export const truncate = (str, max, ellipsis = '…') =>
   str.length <= max ? str : str.slice(0, max - ellipsis.length) + ellipsis;
+
+export const truncate = (str, max, ellipsis = '…') =>
+  str.length <= max ? str : str.slice(0, max - ellipsis.length) + ellipsis;
