cbeb69bc643cbe8b208f99570bbe461eba47e3e8
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-07-07 15:53:21 +1000

    feat: add input validation layer for user payloads

--- src/helpers.js
+++ src/helpers.js
@@ -470,3 +470,5 @@ export const memoize = (fn) => {
 };
 
 export const compose = (...fns) => (x) => fns.reduceRight((v, f) => f(v), x);
+
+export const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
