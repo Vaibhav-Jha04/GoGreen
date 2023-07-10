eb2f6b9701aa633a299a04337efbbfb287c928a4
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-07-10 11:44:29 +1000

    feat: add two-factor authentication flow

--- src/validators.js
+++ src/validators.js
@@ -129,3 +129,5 @@ export const isISODate = (v) => !isNaN(Date.parse(v)) && /^\d{4}-\d{2}-\d{2}/.te
 export const isAlphanumeric = (v) => /^[a-zA-Z0-9]+$/.test(v);
 
 export const isAlphanumeric = (v) => /^[a-zA-Z0-9]+$/.test(v);
+
+export const maxLength = (v, n) => typeof v === 'string' && v.length <= n;
