ddf5a5084b5c0efb78a9f56a2fb9f6947ef9aa04
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-07-10 14:13:38 +1000

    fix: resolve broken import path after directory refactor

--- src/utils.js
+++ src/utils.js
@@ -317,3 +317,5 @@ export const chunk = (arr, size) => {
   for (let i = 0; i < arr.length; i += size) out.push(arr.slice(i, i + size));
   return out;
 };
+
+export const uniq = (arr) => [...new Set(arr)];
