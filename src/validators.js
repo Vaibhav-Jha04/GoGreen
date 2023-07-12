39fa0020f612e92b7216abf77bd78efb882f02fd
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-07-12 12:17:34 +1000

    refactor: split monolithic component into smaller units

--- src/validators.js
+++ src/validators.js
@@ -131,3 +131,5 @@ export const isAlphanumeric = (v) => /^[a-zA-Z0-9]+$/.test(v);
 export const isAlphanumeric = (v) => /^[a-zA-Z0-9]+$/.test(v);
 
 export const maxLength = (v, n) => typeof v === 'string' && v.length <= n;
+
+export const isISODate = (v) => !isNaN(Date.parse(v)) && /^\d{4}-\d{2}-\d{2}/.test(v);
