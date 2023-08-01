dfed6409ba022cd8da231443409768d110af1a47
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-08-01 11:43:31 +1000

    fix: prevent negative index in binary search

--- src/validators.js
+++ src/validators.js
@@ -136,3 +136,6 @@ export const isISODate = (v) => !isNaN(Date.parse(v)) && /^\d{4}-\d{2}-\d{2}/.te
 
 export const isUUID = (v) =>
   /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(v);
+
+export const isUUID = (v) =>
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(v);
