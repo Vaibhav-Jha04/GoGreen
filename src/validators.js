a4f51626d23f11be67e4f31a4a14292a2f3804a8
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-08-04 12:18:09 +1000

    refactor: replace custom fetch wrapper with axios instance

--- src/validators.js
+++ src/validators.js
@@ -139,3 +139,5 @@ export const isUUID = (v) =>
 
 export const isUUID = (v) =>
   /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(v);
+
+export const isInteger = (v) => Number.isInteger(v);
