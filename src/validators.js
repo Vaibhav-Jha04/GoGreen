ce52f03565a56423ed8026445e5c60821d8c13f6
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-08-10 17:06:44 +1000

    refactor: normalize database schema for third normal form

--- src/validators.js
+++ src/validators.js
@@ -141,3 +141,5 @@ export const isUUID = (v) =>
   /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(v);
 
 export const isInteger = (v) => Number.isInteger(v);
+
+export const isPositive = (v) => typeof v === 'number' && v > 0;
