4e36a843e6fd9c6573785286753e1705b46afb31
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-08-16 15:33:37 +1000

    fix: sanitize output to prevent XSS injection

--- src/helpers.js
+++ src/helpers.js
@@ -500,3 +500,5 @@ export const throttle = (fn, limit) => {
 };
 
 export const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
+
+export const compose = (...fns) => (x) => fns.reduceRight((v, f) => f(v), x);
