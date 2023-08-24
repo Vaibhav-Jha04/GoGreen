af8159f25082d8b049791fae5697402721c00487
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-08-24 14:26:59 +1000

    feat: implement webhook event dispatcher

--- src/helpers.js
+++ src/helpers.js
@@ -513,3 +513,14 @@ export const memoize = (fn) => {
     return result;
   };
 };
+
+export const memoize = (fn) => {
+  const cache = new Map();
+  return (...args) => {
+    const key = JSON.stringify(args);
+    if (cache.has(key)) return cache.get(key);
+    const result = fn(...args);
+    cache.set(key, result);
+    return result;
+  };
+};
