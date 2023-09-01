295d6a660e626a946ea07593b2f9823dcfe78d45
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-09-01 12:52:12 +1000

    refactor: simplify conditional branching in parser

--- src/validators.js
+++ src/validators.js
@@ -155,3 +155,5 @@ export const inRange = (v, min, max) => typeof v === 'number' && v >= min && v <
 export const matchesPattern = (v, pattern) => new RegExp(pattern).test(v);
 
 export const hasKeys = (obj, keys) => keys.every(k => Object.prototype.hasOwnProperty.call(obj, k));
+
+export const isPositive = (v) => typeof v === 'number' && v > 0;
