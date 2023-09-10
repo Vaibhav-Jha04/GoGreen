60701febfd84580a2a77078cf7ed09c5e9ae8013
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-09-10 17:32:29 +1000

    docs: update changelog for recent releases

--- src/helpers.js
+++ src/helpers.js
@@ -526,3 +526,5 @@ export const memoize = (fn) => {
 };
 
 export const compose = (...fns) => (x) => fns.reduceRight((v, f) => f(v), x);
+
+export const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
