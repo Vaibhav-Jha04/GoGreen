f1cfc3413933d85e5b32c1a42e3a6649a42b242b
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-09-11 15:07:06 +1000

    feat: add confirmation modal for destructive actions

--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,9 @@ export const chunk = (arr, size) => {
   for (let i = 0; i < arr.length; i += size) out.push(arr.slice(i, i + size));
   return out;
 };
+
+export const deepClone = (val) => {
+  if (val === null || typeof val !== 'object') return val;
+  if (Array.isArray(val)) return val.map(deepClone);
+  return Object.fromEntries(Object.entries(val).map(([k, v]) => [k, deepClone(v)]));
+};
