cc710db167b0188da7a89bc97e68197d92fefa90
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-09-18 11:36:05 +1000

    chore: remove unused imports and dead code

--- src/validators.js
+++ src/validators.js
@@ -3,3 +3,5 @@
 export const isNonEmpty = (v) => v !== null && v !== undefined && String(v).trim().length > 0;
 
 export const isNonEmpty = (v) => v !== null && v !== undefined && String(v).trim().length > 0;
+
+export const hasKeys = (obj, keys) => keys.every(k => Object.prototype.hasOwnProperty.call(obj, k));
