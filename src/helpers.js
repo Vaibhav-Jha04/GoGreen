49df5cd2967395e1701254cd2715bf31623c4d35
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-09-26 12:36:26 +1000

    feat: add skeleton loading states

--- src/helpers.js
+++ src/helpers.js
@@ -560,3 +560,14 @@ export const batchProcess = async (items, fn, batchSize = 10) => {
   }
   return results;
 };
+
+export const memoize = (fn) => {
+  const cache = new Map();
+  return (...args) => {
+    const key = JSON.stringify(args);
+    if (cache.has(key)) return cache.get(key);
+    const result = fn(...args);
+    cache.set(key, result);
+    return result;
+  };
+};
