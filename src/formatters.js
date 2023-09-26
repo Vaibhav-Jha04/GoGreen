8a518919ded897a2f8896c94f5a460b791243ced
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-09-26 14:46:55 +1000

    docs: document environment variable requirements

--- src/formatters.js
+++ src/formatters.js
@@ -231,3 +231,6 @@ export const formatDuration = (ms) => {
 
 export const toTitleCase = (str) =>
   str.replace(/\w\S*/g, (w) => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase());
+
+export const pluralize = (word, count, plural) =>
+  count === 1 ? word : (plural || word + 's');
