56042cc22ec12379a078b33f9d72da060aa98c75
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-10-06 11:17:40 +1000

    feat: add activity logging middleware

--- src/hooks.js
+++ src/hooks.js
@@ -694,3 +694,11 @@ export const useDebounce = (value, delay) => {
   }, [value, delay]);
   return debounced;
 };
+
+import { useState, useCallback } from 'react';
+
+export const useToggle = (initial = false) => {
+  const [state, setState] = useState(initial);
+  const toggle = useCallback(() => setState(s => !s), []);
+  return [state, toggle];
+};
