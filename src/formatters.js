f22aaa7567851697cf5447c8fac8bcdf7fc7ff1c
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-10-08 14:54:09 +1000

    feat: add audit trail for admin actions

--- src/formatters.js
+++ src/formatters.js
@@ -255,3 +255,6 @@ export const pluralize = (word, count, plural) =>
 
 export const truncate = (str, max, ellipsis = '…') =>
   str.length <= max ? str : str.slice(0, max - ellipsis.length) + ellipsis;
+
+export const formatCurrency = (amount, currency = 'USD', locale = 'en-US') =>
+  new Intl.NumberFormat(locale, { style: 'currency', currency }).format(amount);
