51536333d3d785b5372f8470e8987682a71ad04d
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-10-09 11:09:46 +1000

    feat: add CSV import with validation

--- src/formatters.js
+++ src/formatters.js
@@ -258,3 +258,6 @@ export const truncate = (str, max, ellipsis = '…') =>
 
 export const formatCurrency = (amount, currency = 'USD', locale = 'en-US') =>
   new Intl.NumberFormat(locale, { style: 'currency', currency }).format(amount);
+
+export const formatCurrency = (amount, currency = 'USD', locale = 'en-US') =>
+  new Intl.NumberFormat(locale, { style: 'currency', currency }).format(amount);
