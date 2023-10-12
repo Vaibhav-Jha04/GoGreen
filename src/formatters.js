fbd88b00a3b7554e5a9d6f2be4ab37c27602491e
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-10-12 13:00:25 +1000

    fix: handle missing locale fallback in i18n loader

--- src/formatters.js
+++ src/formatters.js
@@ -276,3 +276,6 @@ export const formatNumber = (n, decimals = 2) =>
 
 export const toTitleCase = (str) =>
   str.replace(/\w\S*/g, (w) => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase());
+
+export const toTitleCase = (str) =>
+  str.replace(/\w\S*/g, (w) => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase());
