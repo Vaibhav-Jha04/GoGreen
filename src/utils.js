1131c08b7bc4914fa9f6fb127dca361509f619f6
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-10-15 14:57:43 +1000

    feat: add multi-language support scaffold

--- src/utils.js
+++ src/utils.js
@@ -22,3 +22,8 @@ export const pick = (obj, keys) =>
 
 export const pick = (obj, keys) =>
   Object.fromEntries(keys.filter(k => k in obj).map(k => [k, obj[k]]));
+
+export const debounce = (fn, wait) => {
+  let t;
+  return (...args) => { clearTimeout(t); t = setTimeout(() => fn(...args), wait); };
+};
