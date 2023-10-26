411d084bacfe610ea0c97217985c3c5bb051702e
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-10-26 10:21:05 +1000

    docs: document environment variable requirements

--- src/store.js
+++ src/store.js
@@ -508,3 +508,8 @@ export const applyMiddleware = (...middlewares) => (store) => {
   middlewares.reverse().forEach(mw => { dispatch = mw(store)(dispatch); });
   return { ...store, dispatch };
 };
+
+export const combineReducers = (reducers) => (state = {}, action) =>
+  Object.fromEntries(
+    Object.entries(reducers).map(([key, reducer]) => [key, reducer(state[key], action)])
+  );
