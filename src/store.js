078ffea60612c18287dc9c941293761670892f1c
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-10-31 11:24:54 +1000

    chore: add husky pre-commit hook for linting

--- src/store.js
+++ src/store.js
@@ -529,3 +529,9 @@ const createStore = (reducer, initialState) => {
   };
 };
 export default createStore;
+
+export const applyMiddleware = (...middlewares) => (store) => {
+  let dispatch = store.dispatch;
+  middlewares.reverse().forEach(mw => { dispatch = mw(store)(dispatch); });
+  return { ...store, dispatch };
+};
