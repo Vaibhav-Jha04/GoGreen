340093be57022a9629c1b4623d65c3bde481c230
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-11-14 11:56:20 +1000

    chore: remove unused imports and dead code

--- src/store.js
+++ src/store.js
@@ -568,3 +568,14 @@ export const createSlice = (name, initialState, reducers) => {
   };
   return { actions, reducer };
 };
+
+export const createSlice = (name, initialState, reducers) => {
+  const actions = Object.fromEntries(
+    Object.keys(reducers).map(key => [key, (payload) => ({ type: `${name}/${key}`, payload })])
+  );
+  const reducer = (state = initialState, action) => {
+    const handler = reducers[action.type?.split('/')[1]];
+    return handler ? handler(state, action) : state;
+  };
+  return { actions, reducer };
+};
