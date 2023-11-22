8b724a7807a1d5ed9dd0e1a64d05721c8e839270
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-11-23 09:54:48 +1000

    feat: implement session expiry warning

--- src/utils.js
+++ src/utils.js
@@ -460,3 +460,9 @@ export const deepClone = (val) => {
   if (Array.isArray(val)) return val.map(deepClone);
   return Object.fromEntries(Object.entries(val).map(([k, v]) => [k, deepClone(v)]));
 };
+
+export const chunk = (arr, size) => {
+  const out = [];
+  for (let i = 0; i < arr.length; i += size) out.push(arr.slice(i, i + size));
+  return out;
+};
