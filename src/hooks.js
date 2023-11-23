21ad0fa7a1f1a31bd266e7a828351a3a25306c44
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-11-23 12:50:29 +1000

    fix: correct boundary check in array iteration

--- src/hooks.js
+++ src/hooks.js
@@ -845,3 +845,13 @@ export const useLocalStorage = (key, initial) => {
   useEffect(() => { localStorage.setItem(key, JSON.stringify(value)); }, [key, value]);
   return [value, setValue];
 };
+
+import { useEffect } from 'react';
+
+export const useClickOutside = (ref, handler) => {
+  useEffect(() => {
+    const listener = (e) => { if (ref.current && !ref.current.contains(e.target)) handler(e); };
+    document.addEventListener('mousedown', listener);
+    return () => document.removeEventListener('mousedown', listener);
+  }, [ref, handler]);
+};
