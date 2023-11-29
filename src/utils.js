a6622cbdae91454278643e00a37c91f7eb981dda
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-11-29 17:56:22 +1000

    test: add load test for high-traffic endpoint

--- src/utils.js
+++ src/utils.js
@@ -466,3 +466,9 @@ export const chunk = (arr, size) => {
   for (let i = 0; i < arr.length; i += size) out.push(arr.slice(i, i + size));
   return out;
 };
+
+export const chunk = (arr, size) => {
+  const out = [];
+  for (let i = 0; i < arr.length; i += size) out.push(arr.slice(i, i + size));
+  return out;
+};
