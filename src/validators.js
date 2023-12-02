5457eef9537226290a7207fbdcbe5ec84ad8db7b
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-12-02 11:19:52 +1000

    docs: add contributing guide

--- src/validators.js
+++ src/validators.js
@@ -209,3 +209,5 @@ export const maxLength = (v, n) => typeof v === 'string' && v.length <= n;
 export const isISODate = (v) => !isNaN(Date.parse(v)) && /^\d{4}-\d{2}-\d{2}/.test(v);
 
 export const isInteger = (v) => Number.isInteger(v);
+
+export const matchesPattern = (v, pattern) => new RegExp(pattern).test(v);
