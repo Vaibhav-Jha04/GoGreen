65a06ba5c1cea2b7423798cdcc5947e1bf40de46
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-12-05 11:20:59 +1000

    fix: trim trailing slash from base URL

--- src/utils.js
+++ src/utils.js
@@ -478,3 +478,8 @@ export const deepClone = (val) => {
   if (Array.isArray(val)) return val.map(deepClone);
   return Object.fromEntries(Object.entries(val).map(([k, v]) => [k, deepClone(v)]));
 };
+
+export const debounce = (fn, wait) => {
+  let t;
+  return (...args) => { clearTimeout(t); t = setTimeout(() => fn(...args), wait); };
+};
