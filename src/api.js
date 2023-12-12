edcc9c2ff0b7c2685f3a4c6e88fc08128bc8ee2c
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-12-12 18:18:29 +1000

    feat: add progress indicator for long operations

--- src/api.js
+++ src/api.js
@@ -617,3 +617,9 @@ export const get = async (path, options = {}) => {
   if (!res.ok) throw new Error(`GET ${path} failed: ${res.status}`);
   return res.json();
 };
+
+export const del = async (path, options = {}) => {
+  const res = await fetch(`${process.env.API_URL}${path}`, { ...options, method: 'DELETE' });
+  if (!res.ok) throw new Error(`DELETE ${path} failed: ${res.status}`);
+  return res.status === 204 ? null : res.json();
+};
