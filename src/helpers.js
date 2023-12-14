629fedd3a111ca79bdae1035ff6c9da1bf0ca615
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-12-14 14:55:10 +1000

    feat: implement virtual list for large datasets

--- src/helpers.js
+++ src/helpers.js
@@ -712,3 +712,11 @@ export const batchProcess = async (items, fn, batchSize = 10) => {
   }
   return results;
 };
+
+export const retry = async (fn, retries = 3, delay = 500) => {
+  try { return await fn(); } catch (err) {
+    if (retries <= 0) throw err;
+    await new Promise(r => setTimeout(r, delay));
+    return retry(fn, retries - 1, delay * 2);
+  }
+};
