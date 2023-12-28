194956cc5af70813f41f02646ec31e0dee9fe5c9
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2023-12-28 14:47:49 +1000

    fix: stop stale cache from serving outdated responses

--- src/store.js
+++ src/store.js
@@ -623,3 +623,14 @@ export const combineReducers = (reducers) => (state = {}, action) =>
   Object.fromEntries(
     Object.entries(reducers).map(([key, reducer]) => [key, reducer(state[key], action)])
   );
+
+export const createSlice = (name, initialState, reducers) => {
+  const actions = Object.fromEntries(
+    Object.keys(reducers).map(key => [key, (payload) => ({ type: `${name}/${key}`, payload })])
+  );
+  const reducer = (state = initialState, action) => {
+    const handler = reducers[action.type?.split('/')[1]];
+    return handler ? handler(state, action) : state;
+  };
+  return { actions, reducer };
+};
