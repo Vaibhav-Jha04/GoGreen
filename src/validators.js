caa54742284869a2630716e6d4675fc519c4f358
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-01-19 18:44:49 +1000

    feat: add input validation layer for user payloads

--- src/validators.js
+++ src/validators.js
@@ -232,3 +232,5 @@ export const isUUID = (v) =>
   /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(v);
 
 export const hasKeys = (obj, keys) => keys.every(k => Object.prototype.hasOwnProperty.call(obj, k));
+
+export const inRange = (v, min, max) => typeof v === 'number' && v >= min && v <= max;
