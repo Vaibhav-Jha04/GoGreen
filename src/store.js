58f0af7572f09b61c2f49819dfb81d76ec7fc5a8
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-02-07 19:19:43 +1000

    feat: implement role-based access control

--- src/store.js
+++ src/store.js
@@ -690,3 +690,14 @@ export const applyMiddleware = (...middlewares) => (store) => {
   middlewares.reverse().forEach(mw => { dispatch = mw(store)(dispatch); });
   return { ...store, dispatch };
 };
+
+export const createSlice = (name, initialState, reducers) => {
+  const actions = Object.fromEntries(
+    Object.keys(reducers).map(key => [key, (payload) => ({ type: `${name}/${key}`, payload })])
+  );
+  const reducer = (state = initialState, action) => {
+    const handler = reducers[action.type?.split('/')[1]];
+    return handler ? handler(state, action) : state;
+  };
+  return { actions, reducer };
+};
