f38f1b162b333deca93013dbd9856f3599265266
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-02-09 16:57:11 +1000

    chore: remove unused imports and dead code

--- src/utils.js
+++ src/utils.js
@@ -508,3 +508,8 @@ export const chunk = (arr, size) => {
   for (let i = 0; i < arr.length; i += size) out.push(arr.slice(i, i + size));
   return out;
 };
+
+export const debounce = (fn, wait) => {
+  let t;
+  return (...args) => { clearTimeout(t); t = setTimeout(() => fn(...args), wait); };
+};
