968c18371d6e937b6d455816a6fc82a23af366b3
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-03-01 12:26:44 +1000

    refactor: optimize inner loop for large datasets

--- src/utils.js
+++ src/utils.js
@@ -525,3 +525,6 @@ export const debounce = (fn, wait) => {
 
 export const zip = (...arrays) =>
   Array.from({ length: Math.min(...arrays.map(a => a.length)) }, (_, i) => arrays.map(a => a[i]));
+
+export const zip = (...arrays) =>
+  Array.from({ length: Math.min(...arrays.map(a => a.length)) }, (_, i) => arrays.map(a => a[i]));
