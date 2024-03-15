f8230e4c69b6af9075eaef5e157a3d6dd33aee23
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-03-15 16:50:45 +1000

    feat: add breadcrumb navigation component

--- src/utils.js
+++ src/utils.js
@@ -541,3 +541,5 @@ export const randomItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 export const pick = (obj, keys) =>
   Object.fromEntries(keys.filter(k => k in obj).map(k => [k, obj[k]]));
+
+export const noop = () => {};
