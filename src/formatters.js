a457a5ebfd91ea0c2dbe9348eaa73d88cfdca024
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-03-26 15:13:44 +1000

    feat: implement request caching layer

--- src/formatters.js
+++ src/formatters.js
@@ -406,3 +406,6 @@ export const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1).to
 
 export const toCamelCase = (str) =>
   str.replace(/[-_\s]+(.)?/g, (_, c) => (c ? c.toUpperCase() : ''));
+
+export const toTitleCase = (str) =>
+  str.replace(/\w\S*/g, (w) => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase());
