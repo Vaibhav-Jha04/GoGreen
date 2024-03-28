a56ddb6b4d2ab2ed95478b049c57771302bf58ba
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-03-28 18:31:51 +1000

    feat: add progress indicator for long operations

--- src/store.js
+++ src/store.js
@@ -790,3 +790,14 @@ const createStore = (reducer, initialState) => {
   };
 };
 export default createStore;
+
+export const createSlice = (name, initialState, reducers) => {
+  const actions = Object.fromEntries(
+    Object.keys(reducers).map(key => [key, (payload) => ({ type: `${name}/${key}`, payload })])
+  );
+  const reducer = (state = initialState, action) => {
+    const handler = reducers[action.type?.split('/')[1]];
+    return handler ? handler(state, action) : state;
+  };
+  return { actions, reducer };
+};
