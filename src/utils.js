291aa8845a7d9076fa5ea9f7d77ea133bb4f881d
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-04-08 11:04:49 +1000

    test: add edge case coverage for validator functions

--- src/utils.js
+++ src/utils.js
@@ -564,3 +564,8 @@ export const uniq = (arr) => [...new Set(arr)];
 
 export const omit = (obj, keys) =>
   Object.fromEntries(Object.entries(obj).filter(([k]) => !keys.includes(k)));
+
+export const debounce = (fn, wait) => {
+  let t;
+  return (...args) => { clearTimeout(t); t = setTimeout(() => fn(...args), wait); };
+};
