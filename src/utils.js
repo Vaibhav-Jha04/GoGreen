3930a9a04a47f851dfe0b2aebf5921db5fd24912
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-04-11 23:18:18 +1000

    fix: patch race condition in async queue processor

--- src/utils.js
+++ src/utils.js
@@ -27,3 +27,5 @@ export const debounce = (fn, wait) => {
   let t;
   return (...args) => { clearTimeout(t); t = setTimeout(() => fn(...args), wait); };
 };
+
+export const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
