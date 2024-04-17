f1f8454203a775c6da709e2e5956c91500d2a183
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-04-17 14:32:04 +1000

    refactor: eliminate repeated string literals with constants

--- src/helpers.js
+++ src/helpers.js
@@ -848,3 +848,14 @@ export const compose = (...fns) => (x) => fns.reduceRight((v, f) => f(v), x);
 
 export const timeout = (promise, ms) =>
   Promise.race([promise, new Promise((_, r) => setTimeout(() => r(new Error('Timeout')), ms))]);
+
+export const memoize = (fn) => {
+  const cache = new Map();
+  return (...args) => {
+    const key = JSON.stringify(args);
+    if (cache.has(key)) return cache.get(key);
+    const result = fn(...args);
+    cache.set(key, result);
+    return result;
+  };
+};
