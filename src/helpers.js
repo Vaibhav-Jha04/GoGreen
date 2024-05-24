f86be789485a09105b598e8089cbab5677740e9d
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-05-24 16:02:26 +1000

    docs: update changelog for recent releases

--- src/helpers.js
+++ src/helpers.js
@@ -914,3 +914,10 @@ export const groupBy = (arr, key) =>
     (acc[g] = acc[g] || []).push(item);
     return acc;
   }, {});
+
+export const throttle = (fn, limit) => {
+  let inThrottle;
+  return (...args) => {
+    if (!inThrottle) { fn(...args); inThrottle = true; setTimeout(() => (inThrottle = false), limit); }
+  };
+};
