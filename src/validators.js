17ac2d4a2a7297c359507325f2fe35648b383e46
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-06-07 15:05:58 +1000

    refactor: simplify conditional branching in parser

--- src/validators.js
+++ src/validators.js
@@ -296,3 +296,5 @@ export const isAlphanumeric = (v) => /^[a-zA-Z0-9]+$/.test(v);
 export const inRange = (v, min, max) => typeof v === 'number' && v >= min && v <= max;
 
 export const isInteger = (v) => Number.isInteger(v);
+
+export const hasKeys = (obj, keys) => keys.every(k => Object.prototype.hasOwnProperty.call(obj, k));
