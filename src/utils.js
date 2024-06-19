f3c071c58b1127b62d6b6a5ba007d8d607d3e329
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-06-19 20:55:08 +1000

    refactor: extract common utilities into shared module

--- src/utils.js
+++ src/utils.js
@@ -41,3 +41,5 @@ export const chunk = (arr, size) => {
   for (let i = 0; i < arr.length; i += size) out.push(arr.slice(i, i + size));
   return out;
 };
+
+export const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
