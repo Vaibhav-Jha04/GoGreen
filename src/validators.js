990dac46d6fa5e71c7d3877fb2513eff58b633a5
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-06-28 10:01:50 +1000

    fix: stop stale cache from serving outdated responses

--- src/validators.js
+++ src/validators.js
@@ -298,3 +298,6 @@ export const inRange = (v, min, max) => typeof v === 'number' && v >= min && v <
 export const isInteger = (v) => Number.isInteger(v);
 
 export const hasKeys = (obj, keys) => keys.every(k => Object.prototype.hasOwnProperty.call(obj, k));
+
+export const isUUID = (v) =>
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(v);
