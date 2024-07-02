213efb6eee9ef2c509345346ba22ef4a8ad756d1
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-07-02 19:23:36 +1000

    refactor: move shared types to dedicated types file

--- src/helpers.js
+++ src/helpers.js
@@ -1019,3 +1019,10 @@ export const memoize = (fn) => {
 };
 
 export const compose = (...fns) => (x) => fns.reduceRight((v, f) => f(v), x);
+
+export const groupBy = (arr, key) =>
+  arr.reduce((acc, item) => {
+    const g = typeof key === 'function' ? key(item) : item[key];
+    (acc[g] = acc[g] || []).push(item);
+    return acc;
+  }, {});
