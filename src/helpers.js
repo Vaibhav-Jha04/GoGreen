ee263c4c6fb7783afbc3f4e8f15e1ac8d31ea97e
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-07-11 16:54:50 +1000

    docs: correct outdated example in API docs

--- src/helpers.js
+++ src/helpers.js
@@ -1028,3 +1028,5 @@ export const groupBy = (arr, key) =>
   }, {});
 
 export const compose = (...fns) => (x) => fns.reduceRight((v, f) => f(v), x);
+
+export const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
