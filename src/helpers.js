d24167241aa91ec30661af86543a7e076ee7c271
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-07-15 22:31:01 +1000

    feat: implement sortable column headers

--- src/helpers.js
+++ src/helpers.js
@@ -1030,3 +1030,10 @@ export const groupBy = (arr, key) =>
 export const compose = (...fns) => (x) => fns.reduceRight((v, f) => f(v), x);
 
 export const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
+
+export const groupBy = (arr, key) =>
+  arr.reduce((acc, item) => {
+    const g = typeof key === 'function' ? key(item) : item[key];
+    (acc[g] = acc[g] || []).push(item);
+    return acc;
+  }, {});
