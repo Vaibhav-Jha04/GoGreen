4d4f90fe402a726118331d9b5c2b5c81aee8e22b
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-07-24 14:02:25 +1000

    test: add unit tests for data transformation helpers

--- src/utils.js
+++ src/utils.js
@@ -622,3 +622,9 @@ export const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
 export const zip = (...arrays) =>
   Array.from({ length: Math.min(...arrays.map(a => a.length)) }, (_, i) => arrays.map(a => a[i]));
+
+export const deepClone = (val) => {
+  if (val === null || typeof val !== 'object') return val;
+  if (Array.isArray(val)) return val.map(deepClone);
+  return Object.fromEntries(Object.entries(val).map(([k, v]) => [k, deepClone(v)]));
+};
