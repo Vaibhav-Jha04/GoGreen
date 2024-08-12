7dc8dc5707eb2cac032d0ef3e112e65503f94b33
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-08-12 14:03:50 +1000

    chore: set up path aliases in tsconfig

--- src/helpers.js
+++ src/helpers.js
@@ -1113,3 +1113,8 @@ export const memoize = (fn) => {
     return result;
   };
 };
+
+export const once = (fn) => {
+  let called = false, result;
+  return (...args) => { if (!called) { called = true; result = fn(...args); } return result; };
+};
