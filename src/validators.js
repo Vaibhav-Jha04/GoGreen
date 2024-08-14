601e696f4ee4c4f9e71e4b10ae3440686ff7ec9a
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-08-14 12:14:40 +1000

    feat: add breadcrumb navigation component

--- src/validators.js
+++ src/validators.js
@@ -326,3 +326,5 @@ export const isUrl = (v) => { try { new URL(v); return true; } catch { return fa
 
 export const isUUID = (v) =>
   /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(v);
+
+export const isInteger = (v) => Number.isInteger(v);
