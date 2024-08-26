28749ed1ce0551012eecdb6a096fd8e6ca3346c0
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-08-26 10:09:48 +1000

    docs: update changelog for recent releases

--- src/store.js
+++ src/store.js
@@ -1110,3 +1110,8 @@ export const combineReducers = (reducers) => (state = {}, action) =>
   Object.fromEntries(
     Object.entries(reducers).map(([key, reducer]) => [key, reducer(state[key], action)])
   );
+
+export const combineReducers = (reducers) => (state = {}, action) =>
+  Object.fromEntries(
+    Object.entries(reducers).map(([key, reducer]) => [key, reducer(state[key], action)])
+  );
