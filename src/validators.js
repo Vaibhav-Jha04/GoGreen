1fdea38948c0414b17b4e68e73f81837375ec4a4
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-09-04 11:23:51 +1000

    test: add edge case coverage for validator functions

--- src/validators.js
+++ src/validators.js
@@ -345,3 +345,5 @@ export const isUUID = (v) =>
   /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(v);
 
 export const isAlphanumeric = (v) => /^[a-zA-Z0-9]+$/.test(v);
+
+export const hasKeys = (obj, keys) => keys.every(k => Object.prototype.hasOwnProperty.call(obj, k));
