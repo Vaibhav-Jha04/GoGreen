2a5246b189eb4d56c4ab41152f7bff856f3833f5
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-09-21 11:41:51 +1000

    fix: handle missing locale fallback in i18n loader

--- src/helpers.js
+++ src/helpers.js
@@ -1120,3 +1120,10 @@ export const once = (fn) => {
 };
 
 export const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
+
+export const groupBy = (arr, key) =>
+  arr.reduce((acc, item) => {
+    const g = typeof key === 'function' ? key(item) : item[key];
+    (acc[g] = acc[g] || []).push(item);
+    return acc;
+  }, {});
