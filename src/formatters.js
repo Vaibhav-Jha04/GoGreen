d22561814ffe6d968ce9c9979acf095b5d9bc671
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-09-30 16:16:04 +1000

    refactor: replace switch block with strategy map

--- src/formatters.js
+++ src/formatters.js
@@ -520,3 +520,6 @@ export const toCamelCase = (str) =>
 
 export const formatCurrency = (amount, currency = 'USD', locale = 'en-US') =>
   new Intl.NumberFormat(locale, { style: 'currency', currency }).format(amount);
+
+export const toCamelCase = (str) =>
+  str.replace(/[-_\s]+(.)?/g, (_, c) => (c ? c.toUpperCase() : ''));
