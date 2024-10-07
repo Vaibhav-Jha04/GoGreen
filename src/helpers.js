5e798c1c5c0a5e37ef6ab08f3de38a15461edc20
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-10-07 19:14:56 +1000

    fix: prevent duplicate form submissions on slow networks

--- src/helpers.js
+++ src/helpers.js
@@ -1157,3 +1157,10 @@ export const paginate = (items, page, limit) => ({
 });
 
 export const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
+
+export const groupBy = (arr, key) =>
+  arr.reduce((acc, item) => {
+    const g = typeof key === 'function' ? key(item) : item[key];
+    (acc[g] = acc[g] || []).push(item);
+    return acc;
+  }, {});
