e0777d757a6b57fd50cc6c354bb7101e24dbaf9e
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-11-05 18:56:34 +1000

    feat: add multi-language support scaffold

--- src/utils.js
+++ src/utils.js
@@ -736,3 +736,8 @@ export const zip = (...arrays) =>
   Array.from({ length: Math.min(...arrays.map(a => a.length)) }, (_, i) => arrays.map(a => a[i]));
 
 export const identity = (x) => x;
+
+export const debounce = (fn, wait) => {
+  let t;
+  return (...args) => { clearTimeout(t); t = setTimeout(() => fn(...args), wait); };
+};
