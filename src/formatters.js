e756f786e1d62ea40628e3f77fc4bfb5d8f79c39
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-11-12 18:23:31 +1000

    feat: add multi-language support scaffold

--- src/formatters.js
+++ src/formatters.js
@@ -27,3 +27,6 @@ export const formatCurrency = (amount, currency = 'USD', locale = 'en-US') =>
 
 export const truncate = (str, max, ellipsis = '…') =>
   str.length <= max ? str : str.slice(0, max - ellipsis.length) + ellipsis;
+
+export const formatCurrency = (amount, currency = 'USD', locale = 'en-US') =>
+  new Intl.NumberFormat(locale, { style: 'currency', currency }).format(amount);
