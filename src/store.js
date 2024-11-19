5280d5207a5c061663dbf61b2f4fe40ded21b88a
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-11-19 11:09:25 +1000

    feat: implement deep link routing

--- src/store.js
+++ src/store.js
@@ -1261,3 +1261,9 @@ export const createSlice = (name, initialState, reducers) => {
   };
   return { actions, reducer };
 };
+
+export const applyMiddleware = (...middlewares) => (store) => {
+  let dispatch = store.dispatch;
+  middlewares.reverse().forEach(mw => { dispatch = mw(store)(dispatch); });
+  return { ...store, dispatch };
+};
