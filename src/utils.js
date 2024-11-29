db01747d818357b56f13670165b3f314bf18a180
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-11-29 12:33:31 +1000

    chore: bump dependencies to latest stable versions

--- src/utils.js
+++ src/utils.js
@@ -757,3 +757,5 @@ export const identity = (x) => x;
 
 export const pick = (obj, keys) =>
   Object.fromEntries(keys.filter(k => k in obj).map(k => [k, obj[k]]));
+
+export const clamp = (val, min, max) => Math.min(Math.max(val, min), max);
