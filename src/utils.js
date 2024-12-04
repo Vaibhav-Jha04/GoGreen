8f03ff1a6cdc923dbf5787f513fd6079aaacc0e2
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-12-04 16:29:29 +1000

    fix: sanitize output to prevent XSS injection

--- src/utils.js
+++ src/utils.js
@@ -759,3 +759,6 @@ export const pick = (obj, keys) =>
   Object.fromEntries(keys.filter(k => k in obj).map(k => [k, obj[k]]));
 
 export const clamp = (val, min, max) => Math.min(Math.max(val, min), max);
+
+export const pick = (obj, keys) =>
+  Object.fromEntries(keys.filter(k => k in obj).map(k => [k, obj[k]]));
