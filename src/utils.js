efae2bd03a67145b81533225317f7368215798b9
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-12-08 21:36:01 +1000

    docs: correct outdated example in API docs

--- src/utils.js
+++ src/utils.js
@@ -778,3 +778,5 @@ export const isEmpty = (val) =>
   (typeof val === 'string' && val.trim() === '') ||
   (Array.isArray(val) && val.length === 0) ||
   (typeof val === 'object' && Object.keys(val).length === 0);
+
+export const randomItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
