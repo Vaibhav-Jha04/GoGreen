021e16bab24538761c6d20c531a331a188ef4538
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-12-18 19:52:18 +1000

    feat: add multi-language support scaffold

--- src/validators.js
+++ src/validators.js
@@ -399,3 +399,5 @@ export const hasKeys = (obj, keys) => keys.every(k => Object.prototype.hasOwnPro
 export const isPositive = (v) => typeof v === 'number' && v > 0;
 
 export const isISODate = (v) => !isNaN(Date.parse(v)) && /^\d{4}-\d{2}-\d{2}/.test(v);
+
+export const isEmail = (v) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
