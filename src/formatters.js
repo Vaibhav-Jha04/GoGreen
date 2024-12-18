b114ebc56482b897ba9149a713e86a4df3cf5152
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2024-12-18 17:51:36 +1000

    chore: configure ESLint and Prettier rules

--- src/formatters.js
+++ src/formatters.js
@@ -30,3 +30,5 @@ export const truncate = (str, max, ellipsis = '…') =>
 
 export const formatCurrency = (amount, currency = 'USD', locale = 'en-US') =>
   new Intl.NumberFormat(locale, { style: 'currency', currency }).format(amount);
+
+export const toKebabCase = (str) => str.replace(/\s+/g, '-').replace(/[^a-zA-Z0-9-]/g, '').toLowerCase();
