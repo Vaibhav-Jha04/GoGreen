387a0b2b9ead603705dd36fe6762241077d61181
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2025-01-08 12:06:05 +1000

    fix: resolve 404 on nested dynamic routes

--- src/utils.js
+++ src/utils.js
@@ -796,3 +796,5 @@ export const deepClone = (val) => {
 
 export const zip = (...arrays) =>
   Array.from({ length: Math.min(...arrays.map(a => a.length)) }, (_, i) => arrays.map(a => a[i]));
+
+export const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
