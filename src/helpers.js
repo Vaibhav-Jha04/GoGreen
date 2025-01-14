4986675b293d13afcc10991859efedcb4da1a857
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2025-01-14 22:05:40 +1000

    feat: add bulk action support to table

--- src/helpers.js
+++ src/helpers.js
@@ -1278,3 +1278,10 @@ export const groupBy = (arr, key) =>
     (acc[g] = acc[g] || []).push(item);
     return acc;
   }, {});
+
+export const throttle = (fn, limit) => {
+  let inThrottle;
+  return (...args) => {
+    if (!inThrottle) { fn(...args); inThrottle = true; setTimeout(() => (inThrottle = false), limit); }
+  };
+};
