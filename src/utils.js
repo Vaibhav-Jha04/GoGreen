3c046c1aa83e059f9cf45b41cf9c10a7224120cf
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2025-01-16 14:33:31 +1000

    refactor: replace custom fetch wrapper with axios instance

--- src/utils.js
+++ src/utils.js
@@ -798,3 +798,5 @@ export const zip = (...arrays) =>
   Array.from({ length: Math.min(...arrays.map(a => a.length)) }, (_, i) => arrays.map(a => a[i]));
 
 export const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+export const randomItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
