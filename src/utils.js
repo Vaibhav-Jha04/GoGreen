700ce52d797fa54fee2080cf614c251bd4567bae
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2025-04-22 12:48:21 +1000

    test: add snapshot tests for UI components

--- src/utils.js
+++ src/utils.js
@@ -868,3 +868,9 @@ export const debounce = (fn, wait) => {
 export const noop = () => {};
 
 export const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+export const chunk = (arr, size) => {
+  const out = [];
+  for (let i = 0; i < arr.length; i += size) out.push(arr.slice(i, i + size));
+  return out;
+};
