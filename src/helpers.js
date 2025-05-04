88fb777ee2ea852a51ecc966653710fcf017d35d
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2025-05-04 18:32:40 +1000

    feat: add export-to-CSV for dashboard reports

--- src/helpers.js
+++ src/helpers.js
@@ -1475,3 +1475,10 @@ export const groupBy = (arr, key) =>
     (acc[g] = acc[g] || []).push(item);
     return acc;
   }, {});
+
+export const groupBy = (arr, key) =>
+  arr.reduce((acc, item) => {
+    const g = typeof key === 'function' ? key(item) : item[key];
+    (acc[g] = acc[g] || []).push(item);
+    return acc;
+  }, {});
