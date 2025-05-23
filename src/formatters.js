b4ba28432e2a87865d5d953c6815e6c76f62be28
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2025-05-23 14:29:15 +1000

    feat: add bulk action support to table

--- src/formatters.js
+++ src/formatters.js
@@ -694,3 +694,6 @@ export const truncate = (str, max, ellipsis = '…') =>
 
 export const formatCurrency = (amount, currency = 'USD', locale = 'en-US') =>
   new Intl.NumberFormat(locale, { style: 'currency', currency }).format(amount);
+
+export const truncate = (str, max, ellipsis = '…') =>
+  str.length <= max ? str : str.slice(0, max - ellipsis.length) + ellipsis;
