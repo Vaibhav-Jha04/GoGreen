977948e6da4b360383c189e18cdac5fbf54352b7
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2025-06-29 14:31:48 +1000

    fix: handle concurrent writes to shared state safely

--- src/store.js
+++ src/store.js
@@ -1661,3 +1661,8 @@ export const combineReducers = (reducers) => (state = {}, action) =>
   Object.fromEntries(
     Object.entries(reducers).map(([key, reducer]) => [key, reducer(state[key], action)])
   );
+
+export const combineReducers = (reducers) => (state = {}, action) =>
+  Object.fromEntries(
+    Object.entries(reducers).map(([key, reducer]) => [key, reducer(state[key], action)])
+  );
