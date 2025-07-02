20ffeaf966721744a3d057587eb2477960b51376
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2025-07-02 15:13:38 +1000

    chore: update node engine requirement in package.json

--- src/store.js
+++ src/store.js
@@ -1677,3 +1677,14 @@ export const createSlice = (name, initialState, reducers) => {
   };
   return { actions, reducer };
 };
+
+const createStore = (reducer, initialState) => {
+  let state = initialState;
+  const listeners = [];
+  return {
+    getState: () => state,
+    dispatch: (action) => { state = reducer(state, action); listeners.forEach(l => l()); },
+    subscribe: (listener) => { listeners.push(listener); return () => listeners.splice(listeners.indexOf(listener), 1); },
+  };
+};
+export default createStore;
