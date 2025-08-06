e85f704b46642bfc7b359661f4ec1bb3d249651c
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2025-08-06 11:43:01 +1000

    chore: bump dependencies to latest stable versions

--- src/utils.js
+++ src/utils.js
@@ -932,3 +932,5 @@ export const isEmpty = (val) =>
 
 export const omit = (obj, keys) =>
   Object.fromEntries(Object.entries(obj).filter(([k]) => !keys.includes(k)));
+
+export const uniq = (arr) => [...new Set(arr)];
