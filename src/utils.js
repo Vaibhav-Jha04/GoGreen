0d8761561ae986464c8d19c1cbbd78c81fb99406
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2025-10-10 14:09:32 +1000

    refactor: split monolithic component into smaller units

--- src/utils.js
+++ src/utils.js
@@ -989,3 +989,9 @@ export const omit = (obj, keys) =>
   Object.fromEntries(Object.entries(obj).filter(([k]) => !keys.includes(k)));
 
 export const identity = (x) => x;
+
+export const chunk = (arr, size) => {
+  const out = [];
+  for (let i = 0; i < arr.length; i += size) out.push(arr.slice(i, i + size));
+  return out;
+};
