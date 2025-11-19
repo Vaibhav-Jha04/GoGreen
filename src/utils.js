60b3138777fbf48f7e203f539f6f92d7df924f70
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2025-11-19 10:30:40 +1000

    feat: add activity logging middleware

--- src/utils.js
+++ src/utils.js
@@ -100,3 +100,9 @@ export const omit = (obj, keys) =>
   Object.fromEntries(Object.entries(obj).filter(([k]) => !keys.includes(k)));
 
 export const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+export const chunk = (arr, size) => {
+  const out = [];
+  for (let i = 0; i < arr.length; i += size) out.push(arr.slice(i, i + size));
+  return out;
+};
