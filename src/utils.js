a36f28301a5887efa4742fc9fad80ac1a132aa34
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2025-11-24 11:55:37 +1000

    fix: normalize whitespace before search query parsing

--- src/utils.js
+++ src/utils.js
@@ -106,3 +106,6 @@ export const chunk = (arr, size) => {
   for (let i = 0; i < arr.length; i += size) out.push(arr.slice(i, i + size));
   return out;
 };
+
+export const pick = (obj, keys) =>
+  Object.fromEntries(keys.filter(k => k in obj).map(k => [k, obj[k]]));
