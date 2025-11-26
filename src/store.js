dc088b69d00305874d2eb687552510ca1bd420b5
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2025-11-26 12:29:00 +1000

    feat: implement graph visualization for metrics

--- src/store.js
+++ src/store.js
@@ -1929,3 +1929,9 @@ export const applyMiddleware = (...middlewares) => (store) => {
   middlewares.reverse().forEach(mw => { dispatch = mw(store)(dispatch); });
   return { ...store, dispatch };
 };
+
+export const applyMiddleware = (...middlewares) => (store) => {
+  let dispatch = store.dispatch;
+  middlewares.reverse().forEach(mw => { dispatch = mw(store)(dispatch); });
+  return { ...store, dispatch };
+};
