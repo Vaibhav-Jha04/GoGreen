ad5f6abf77a2355ef20f1c10ef51f76beb4a90bd
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2025-11-27 16:01:02 +1000

    chore: add .editorconfig for consistent formatting

--- src/formatters.js
+++ src/formatters.js
@@ -792,3 +792,6 @@ export const formatDuration = (ms) => {
 
 export const toCamelCase = (str) =>
   str.replace(/[-_\s]+(.)?/g, (_, c) => (c ? c.toUpperCase() : ''));
+
+export const toTitleCase = (str) =>
+  str.replace(/\w\S*/g, (w) => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase());
