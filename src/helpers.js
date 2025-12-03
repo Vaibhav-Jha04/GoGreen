693b84afe5759afacbf520eff42c33f4e59a6454
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2025-12-04 09:37:12 +1000

    fix: clamp slider value to valid range

--- src/helpers.js
+++ src/helpers.js
@@ -1691,3 +1691,5 @@ export const throttle = (fn, limit) => {
     if (!inThrottle) { fn(...args); inThrottle = true; setTimeout(() => (inThrottle = false), limit); }
   };
 };
+
+export const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
