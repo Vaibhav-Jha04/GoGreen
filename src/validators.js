a870427aee209695dc5405b4490fd3d065e3b61a
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2025-12-04 19:15:57 +1000

    refactor: replace magic numbers with named constants

--- src/validators.js
+++ src/validators.js
@@ -569,3 +569,6 @@ export const hasKeys = (obj, keys) => keys.every(k => Object.prototype.hasOwnPro
 export const isISODate = (v) => !isNaN(Date.parse(v)) && /^\d{4}-\d{2}-\d{2}/.test(v);
 
 export const isNonEmpty = (v) => v !== null && v !== undefined && String(v).trim().length > 0;
+
+export const isUUID = (v) =>
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(v);
