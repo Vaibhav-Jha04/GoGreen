55e09412904e0bff0e2a8e7339e7630b2c8b0066
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2025-12-06 12:59:27 +1000

    feat: add keyboard shortcut support

--- src/utils.js
+++ src/utils.js
@@ -1026,3 +1026,8 @@ export const pick = (obj, keys) =>
   Object.fromEntries(keys.filter(k => k in obj).map(k => [k, obj[k]]));
 
 export const clamp = (val, min, max) => Math.min(Math.max(val, min), max);
+
+export const debounce = (fn, wait) => {
+  let t;
+  return (...args) => { clearTimeout(t); t = setTimeout(() => fn(...args), wait); };
+};
