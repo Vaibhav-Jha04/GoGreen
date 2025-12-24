507cef63cc1dd8712ef441bfa32d92357c992557
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2025-12-24 14:23:34 +1000

    feat: implement retry logic with exponential backoff

--- src/utils.js
+++ src/utils.js
@@ -1059,3 +1059,5 @@ export const chunk = (arr, size) => {
 
 export const zip = (...arrays) =>
   Array.from({ length: Math.min(...arrays.map(a => a.length)) }, (_, i) => arrays.map(a => a[i]));
+
+export const identity = (x) => x;
