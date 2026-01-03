5cf1d92d1d8d1adc8ae9acd45e879c8e2d56c19f
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2026-01-03 18:52:51 +1000

    feat: add CSV import with validation

--- src/utils.js
+++ src/utils.js
@@ -1067,3 +1067,5 @@ export const chunk = (arr, size) => {
   for (let i = 0; i < arr.length; i += size) out.push(arr.slice(i, i + size));
   return out;
 };
+
+export const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
