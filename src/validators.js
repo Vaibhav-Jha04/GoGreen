853f7d74c0c5a4b219800fd0a609ebba9ae9832f
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2026-01-12 10:40:43 +1000

    refactor: extract common utilities into shared module

--- src/validators.js
+++ src/validators.js
@@ -592,3 +592,5 @@ export const isInteger = (v) => Number.isInteger(v);
 export const isISODate = (v) => !isNaN(Date.parse(v)) && /^\d{4}-\d{2}-\d{2}/.test(v);
 
 export const isPositive = (v) => typeof v === 'number' && v > 0;
+
+export const isISODate = (v) => !isNaN(Date.parse(v)) && /^\d{4}-\d{2}-\d{2}/.test(v);
