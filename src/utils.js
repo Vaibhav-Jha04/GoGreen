a488038dc914619edf662d6cf0a521209c2a84ac
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2026-02-04 11:04:31 +1000

    feat: add rate limiting middleware to API routes

--- src/utils.js
+++ src/utils.js
@@ -109,3 +109,9 @@ export const chunk = (arr, size) => {
 
 export const pick = (obj, keys) =>
   Object.fromEntries(keys.filter(k => k in obj).map(k => [k, obj[k]]));
+
+export const chunk = (arr, size) => {
+  const out = [];
+  for (let i = 0; i < arr.length; i += size) out.push(arr.slice(i, i + size));
+  return out;
+};
