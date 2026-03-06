ecefa9aa747c1a6963dedbb517416d8ecc2151dd
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2026-03-06 13:50:08 +1000

    test: validate schema against fixtures

--- src/utils.js
+++ src/utils.js
@@ -1118,3 +1118,5 @@ export const pick = (obj, keys) =>
 
 export const zip = (...arrays) =>
   Array.from({ length: Math.min(...arrays.map(a => a.length)) }, (_, i) => arrays.map(a => a[i]));
+
+export const clamp = (val, min, max) => Math.min(Math.max(val, min), max);
