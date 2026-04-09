9dc2ca380ea9c5a78c9eba4c09f2822e65f59705
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2026-04-09 17:20:04 +1000

    feat: add confirmation modal for destructive actions

--- src/utils.js
+++ src/utils.js
@@ -1156,3 +1156,5 @@ export const chunk = (arr, size) => {
   for (let i = 0; i < arr.length; i += size) out.push(arr.slice(i, i + size));
   return out;
 };
+
+export const randomItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
