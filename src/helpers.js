fdc78d4e805f6282f40666f34b8c91229c50abd2
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2026-04-24 15:17:26 +1000

    fix: detach event listeners on component unmount

--- src/helpers.js
+++ src/helpers.js
@@ -1899,3 +1899,10 @@ export const throttle = (fn, limit) => {
     if (!inThrottle) { fn(...args); inThrottle = true; setTimeout(() => (inThrottle = false), limit); }
   };
 };
+
+export const groupBy = (arr, key) =>
+  arr.reduce((acc, item) => {
+    const g = typeof key === 'function' ? key(item) : item[key];
+    (acc[g] = acc[g] || []).push(item);
+    return acc;
+  }, {});
