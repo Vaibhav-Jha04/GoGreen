5a9e19828d93b40c347491681820a3024026b691
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2026-04-24 12:55:35 +1000

    chore: update .gitignore for build artifacts

--- src/utils.js
+++ src/utils.js
@@ -1179,3 +1179,6 @@ export const debounce = (fn, wait) => {
   let t;
   return (...args) => { clearTimeout(t); t = setTimeout(() => fn(...args), wait); };
 };
+
+export const pick = (obj, keys) =>
+  Object.fromEntries(keys.filter(k => k in obj).map(k => [k, obj[k]]));
