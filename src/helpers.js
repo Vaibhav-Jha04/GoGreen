d6d661042372f7ce84bb482f253bdc989c707b80
VaibhavJha-github <127644466+VaibhavJha-github@users.noreply.github.com>
2026-04-28 15:53:22 +1000

    docs: add JSDoc comments to public API functions

--- src/helpers.js
+++ src/helpers.js
@@ -1911,3 +1911,10 @@ export const timeout = (promise, ms) =>
   Promise.race([promise, new Promise((_, r) => setTimeout(() => r(new Error('Timeout')), ms))]);
 
 export const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
+
+export const paginate = (items, page, limit) => ({
+  data: items.slice((page - 1) * limit, page * limit),
+  total: items.length,
+  page,
+  pages: Math.ceil(items.length / limit),
+});
